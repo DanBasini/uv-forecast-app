767f650c8238d7b16e2a97d60bbdbd4c04967944
Daniel Basini <4006@trinity.croydon.sch.uk>
2025-03-25 20:37:11 +0000

    Initial commit

--- bash.sh
+++ bash.sh
@@ -1,6 +1,6 @@
 git init
 git add .
 git commit -m "Initial commit"
-git remote add origin https://github.com/YOUR_USERNAME/uv-forecast-app.git
+git remote add origin https://github.com/DanBasini/uv-forecast-app.git
 git branch -M main
 git push -u origin main
